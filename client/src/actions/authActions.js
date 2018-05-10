e444035b494e6f1ebbfcbd215c78366b078fa5f8
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-05-10 10:48:13 +0700

    Add header for protective route

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,35 @@
-import {TEST_DISPATCH} from './types'
-
+import axios from 'axios';
+import {GET_ERRORS} from './types'
+import setAuthToken from './../utils/setAuthToken';
 // Register User
-export const registerUser = (userData) => {
-      return {
-        type: TEST_DISPATCH,
-        payload: userData
-      }
+export const registerUser = (userData ,history) => dispatch => {  
+   axios.post('/api/users/register', userData)
+        .then((res)=> history.push('/login'))
+        .catch(err=> 
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+    })
+  );
+}
+
+// Login User - Get User Token
+
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+  .then((res)=> {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set Token to ls
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth Header
+      setAuthToken(token);
+
+  })
+  .catch(err=> 
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+})
+);
 }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './authReducer.js';
+import errorReducer from './errorReducer.js';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  errors: errorReducer
 });
